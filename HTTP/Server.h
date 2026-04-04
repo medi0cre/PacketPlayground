41332a14d2d4c97c427799f51bb130eeaed6c765
medi0cre <anirbanmistry5000@gmail.com>
2026-04-04 11:20:43 -0230

    Added cleanup code to Server.h

--- HTTP/Server.h
+++ HTTP/Server.h
@@ -1,19 +1,52 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <cstdint>
+#include <functional>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 namespace HTTP
 {
+	struct Request
+    {
+        std::string Method = "";
+        std::string URI = "";
+        std::string Version = "";
+        std::unordered_map<std::string, std::string> Headers{};
+        std::string Body = "";
+    };
+	
+	struct Response
+    {
+        uint16_t StatusCode = 200;
+        std::string Status = "OK";
+        std::unordered_map<std::string, std::string> Headers{};
+        std::string Body = "";
+    };
+	
     class Server
     {
         private:
-		    int ConnectionCount = 0;
+			static constexpr uint8_t MaxConnections = 32;
+			static constexpr uint16_t BufferSize = 32768;
+			static constexpr uint16_t ReadBufferSize = 8192;
+		
+		    uint8_t ConnectionCount = 0;
             pollfd* ConnectionList = nullptr;
             char* MessageBuffer = nullptr;
+			std::unordered_map<std::string, std::function<Response(const Request&)>> Routes{};
+			
+			void HandleNewConnection();
+			void HandleClientData(uint8_t Index);
+			void SendResponse(uint8_t ClientFD, const Response& Res);
+			void RemoveConnection(uint8_t Index);
+			Request ParseRequest(const std::string& RawData);
 
         public:
             Server(const char* IPAddress, const char* Port);
             ~Server();
 	    	void Run();
+			void AddRoute(const std::string& Method, const std::string& Path, std::function<Response(const Request&)> Dispatcher);
     };
 }
