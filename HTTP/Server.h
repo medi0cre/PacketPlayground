0f80a141896091f9cab6d5a1d26f812bcd2a223c
medi0cre <anirbanmistry5000@gmail.com>
2026-04-07 20:37:00 -0230

    Added builder

--- HTTP/Server.h
+++ HTTP/Server.h
@@ -8,7 +8,7 @@
 
 namespace HTTP
 {
-	struct Request
+    struct Request
     {
         std::string Method = "";
         std::string URI = "";
@@ -17,7 +17,7 @@ namespace HTTP
         std::string Body = "";
     };
 	
-	struct Response
+    struct Response
     {
         uint16_t StatusCode = 200;
         std::string Status = "OK";
@@ -25,28 +25,44 @@ namespace HTTP
         std::string Body = "";
     };
 	
+    class ResponseBuilder
+    {
+	private:
+	    Response Res{};
+			
+	public:
+	    ResponseBuilder() = default;
+	    ~ResponseBuilder() = default;
+			
+	    ResponseBuilder& Reset();
+	    ResponseBuilder& StatusCode(uint16_t StatusCode);
+	    ResponseBuilder& Status(const std::string& Status);
+	    ResponseBuilder& Body(const std::string& Body);
+	    ResponseBuilder& Header(const std::string& Key, const std::string& Value);
+    };
+	
     class Server
     {
         private:
-			static constexpr uint8_t MaxConnections = 32;
-			static constexpr uint16_t BufferSize = 32768;
-			static constexpr uint16_t ReadBufferSize = 8192;
+	    static constexpr uint8_t MaxConnections = 32;
+	    static constexpr uint16_t BufferSize = 32768;
+	    static constexpr uint16_t ReadBufferSize = 8192;
 		
-		    uint8_t ConnectionCount = 0;
+	    uint8_t ConnectionCount = 0;
             pollfd* ConnectionList = nullptr;
             char* MessageBuffer = nullptr;
-			std::unordered_map<std::string, std::function<Response(const Request&)>> Routes{};
+	    std::unordered_map<std::string, std::function<Response(const Request&)>> Routes{};
 			
-			void HandleNewConnection();
-			void HandleClientData(uint8_t Index);
-			void SendResponse(uint8_t ClientFD, const Response& Res);
-			void RemoveConnection(uint8_t Index);
-			Request ParseRequest(const std::string& RawData);
+	    void HandleNewConnection();
+	    void HandleClientData(uint8_t Index);
+	    void SendResponse(uint8_t ClientFD, const Response& Res);
+	    void RemoveConnection(uint8_t Index);
+	    Request ParseRequest(const std::string& RawData);
 
         public:
             Server(const char* IPAddress, const char* Port);
             ~Server();
-	    	void Run();
-			void AddRoute(const std::string& Method, const std::string& Path, std::function<Response(const Request&)> Dispatcher);
+	    void Run();
+	    void AddRoute(const std::string& Method, const std::string& Path, std::function<Response(const Request&)> Dispatcher);
     };
 }
