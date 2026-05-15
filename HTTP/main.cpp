400f996177364b1d42f35cde150069b472b85a61
medi0cre <anirbanmistry5000@gmail.com>
2026-05-14 23:38:53 -0230

    Added routes for testing

--- HTTP/main.cpp
+++ HTTP/main.cpp
@@ -2,8 +2,29 @@
 
 int main()
 {
-    HTTP::Server* MyServer = new HTTP::Server("127.0.0.1", "8080");
-    MyServer->Run();
-    delete MyServer;
+    HTTP::Server* PPG = new HTTP::Server("127.0.0.1", "8080");
+    
+	PPG->AddRoute("GET", "/", [](const HTTP::Request& Req)
+    {
+        HTTP::ResponseBuilder Builder;
+        return Builder
+            .OK()
+            .HTML()
+            .Body("<h1>Hello World</h1>")
+            .Build();
+    });
+
+    PPG->AddRoute("POST", "/echo", [](const HTTP::Request& Req)
+    {
+        HTTP::ResponseBuilder Builder;
+        return Builder
+            .OK()
+            .JSON()
+            .Body(Req.Body)
+            .Build();
+    });
+	
+	PPG->Run();
+    delete PPG;
     return 0;
 }
